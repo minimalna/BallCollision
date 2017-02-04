4ab09ac44a778a58f5597e75327a6edc6800c2b0
minimalna <zk.malnasi@gmail.com>
2017-02-04 15:38:56 +0200

    Adjusted ball coordinates to Canvas size

--- src/ballscollision/Canvas.java
+++ src/ballscollision/Canvas.java
@@ -10,10 +10,6 @@ public class Canvas extends JPanel {
     private int num = rand.nextInt(27) + 3;
     private ArrayList<Ball> balls = new ArrayList<>();
 
-    Canvas(int width, int height) {
-        init(width, height);
-    }
-
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -21,8 +17,10 @@ public class Canvas extends JPanel {
         drawCircle(g);
     }
 
-    private void init(int width, int height) {
-        for (int i=0; i < num; i++){
+    public void init() {
+        int width = this.getWidth();
+        int height = this.getHeight();
+        for (int i = 0; i < num; i++) {
             int r = rand.nextInt(80) + 20;
             while (2 * r >= width || 2 * r >= height) {
                 r = rand.nextInt(80) + 20;
@@ -37,7 +35,7 @@ public class Canvas extends JPanel {
 
     private void drawCircle(Graphics g) {
         System.out.println("Drawing...");
-        for(Ball b : balls){
+        for (Ball b : balls) {
             int r = b.getR();
             g.drawOval(b.getX() - r, b.getY() - r, 2 * r, 2 * r);
         }
--- src/ballscollision/Window.java
+++ src/ballscollision/Window.java
@@ -3,14 +3,17 @@ package ballscollision;
 import javax.swing.*;
 import java.awt.*;
 
-public class Window extends JFrame{
+public class Window extends JFrame {
     public Window(int width, int height) {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setPreferredSize(new Dimension(width, height));
-        this.setMinimumSize(new Dimension(100,100));
-        setContentPane(new Canvas(width, height));
+        this.setMinimumSize(new Dimension(100, 100));
+        Canvas canv = new Canvas();
+        setContentPane(canv);
         this.pack();
         this.setTitle("Balls Collider");
         this.setVisible(true);
+        System.out.println("Width: " + canv.getWidth() + ", Height: " + canv.getHeight());
+        canv.init();
     }
 }
